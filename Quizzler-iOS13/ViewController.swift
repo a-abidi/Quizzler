93cece263bb80487033cde4a0fd69e53fa8994fe
a-abidi <aliabidi@icloud.com>
2020-08-20 16:42:34 +0100

    Add all questions.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -14,15 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
+        
+    var quizNumber = 0
     
     let quiz = [
-        ["a", "True"],
-        ["b", "True"],
-        ["c", "True"],
-        ["d", "True"]
+        Question(q: "A slug's blood is green.", a: true),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: true),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: true),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: true),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: false),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: true),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: false),
+        Question(q: "Google was originally called 'Backrub'.", a: true),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: true),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: false),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: false),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: true)
     ]
-    
-    var quizNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +40,9 @@ class ViewController: UIViewController {
     
     @IBAction func userResponse(_ sender: UIButton) {
         let answer = sender.currentTitle
-        let actualAnswer = quiz[quizNumber][1]
+        let actualQuestion = quiz[quizNumber]
         
-        if (answer == actualAnswer) {
+        if (answer == "True" && actualQuestion.a || answer == "False" && !actualQuestion.a) {
             print("Correct!")
         } else {
             print("Wrong!")
@@ -50,7 +58,7 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-        question.text = quiz[quizNumber][0]
+        question.text = quiz[quizNumber].q
     }
 
 }
