1c3273b2146ff3a1104f590ad075ac58b6970301
a-abidi <aliabidi@icloud.com>
2020-08-20 16:41:56 +0100

    Created Question struct with fieldsq and a

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Ali on 20/08/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let q: String
+    let a: Bool
+}
